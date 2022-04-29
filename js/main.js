0703ea9e4e31899f85592de9be724e7afb121fa4
Yura <mr.Shapovaloff3101@gmail.com>
2022-04-29 20:57:03 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
+const DESCRIPTIONS = [
+  'Следуй за своим сердцем, но не забывай брать с собой мозг.',
+  'Успех следует за упорным трудом.',
+  'Вперед к новым вершинам.',
+  'Только цель, никаких препятствий.',
+  'Что делать, когда не знаешь, что делать? В любой непонятной ситуации немедленно начинай обнимать кота…',
+  'Люблю хэштеги, они напоминают вафельки',
+  'Ошибки являются доказательством того, что вы пытались.',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Аанг',
+  'Зуко',
+  'Катара',
+  'Сокка',
+  'Айро',
+  'Азула',
+  'Озай',
+  'Азулон',
+  'Мэй',
+  'Джао',
+];
+
+const IMAGES_COUNT = 25;
+
 const getRandomIntInclusive = (min, max) => {
-  if (min >= max || min < 0) {
+  if (max <= min || min < 0) {
     return 'Unexpected data!';
   } else {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(min + Math.random() * (max - min + 1));
   }
 };
 
@@ -12,4 +46,41 @@ const stringLength = (stringCheck, maxLength) => stringCheck.length <= maxLength
 
 stringLength('Привет', 5);
 getRandomIntInclusive(40, 90);
+
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const createComments = (index) => {
+  const comments = [];
+  const commentsAmount = getRandomIntInclusive(2, 4);
+
+  for (let i = 0; i < commentsAmount; i++) {
+    comments.push({
+      id: index * 100 + i,
+      avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,
+      message: getRandomArrayElement(MESSAGES),
+      name: getRandomArrayElement(NAMES),
+    });
+  }
+
+  return comments;
+};
+
+const createImage = (index) => ({
+  id: index + 1,
+  url: `photos/${index + 1}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomIntInclusive(15, 200),
+  comments: createComments(index),
+});
+
+const createImages = () => {
+  const images = [];
+  for (let i = 0; i < IMAGES_COUNT; i ++) {
+    images.push(createImage(i));
+  }
+
+  return images;
+};
+
+createImages();
 
